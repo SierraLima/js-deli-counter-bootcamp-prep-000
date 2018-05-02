fd6c21e35844f3da9eb5da9c5e0fd720afd721be
Stéphane <stephluethi@hotmail.com>
2018-05-02 12:51:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,27 +3,47 @@
 function takeANumber(line,name){
  line.push(name)
  
+<<<<<<< HEAD
 return `Welcome, ${name}. You are number ${line.length} in line.`
 
 }
 
 function nowServing(line){
+=======
+return `Welcome ${name} number: ${line}`
+
+}
+
+function nowServing(){
+>>>>>>> 619dda413fb448f52dc899fbf4823e1ab68eab2d
   
   if (!line.length) {
     return "There is nobody waiting to be served!"
   }
   
+<<<<<<< HEAD
   return `Currently serving ${line.shift()}.`
+=======
+  return `we are currently serving ${line.shift()}`
+>>>>>>> 619dda413fb448f52dc899fbf4823e1ab68eab2d
   
 }
 
 function currentLine(line){
   if (!line.length) {
+<<<<<<< HEAD
     return "The line is currently empty."}
   
   const numbersAndNames = []
   
   for (let i = 0, l = line.length; i < l; i++) {
+=======
+    console.log("The line is currently empty.")}
+  
+  numbersAndNames = []
+  
+  for (i = 0, l = line.length; i < l; i++) {
+>>>>>>> 619dda413fb448f52dc899fbf4823e1ab68eab2d
   numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
   
